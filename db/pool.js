4bf70e4f9849d4afb1fe8b36297cb3012e0debda
beeming2546 <beeming2546@gmail.com>
2025-07-31 10:04:15 +0700

    Initial commit

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,12 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');  // บังคับใช้ IPv4 ก่อน
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // << สำคัญสำหรับ Render + Supabase
+  ssl: { rejectUnauthorized: false } // สำคัญสำหรับ Render + Supabase
 });
 
 module.exports = pool;
