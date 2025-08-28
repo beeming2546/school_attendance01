f7fc121268b8efc21f59623bc57e24a7d49af2a8
beeming2546 <beeming2546@gmail.com>
2025-08-28 17:05:56 +0700

    Initial commit

--- db/pool.js
+++ db/pool.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL, // ดึงมาจากไฟล์ env
   ssl: { rejectUnauthorized: false }, // สำหรับ Render/Supabase
 });
 
